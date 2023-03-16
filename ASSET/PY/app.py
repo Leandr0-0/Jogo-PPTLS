11fc6463ff8068e09c935fe7810077dc5956dd92
Leandro Silva <sr.syllw@gmail.com>
2023-03-15 21:42:55 -0300

    Codando os prints do PY

--- ASSET/PY/app.py
+++ ASSET/PY/app.py
@@ -1,4 +1,14 @@
 from random import randint 
 itens = ('Pedra', 'Papel', 'Tesoura', 'Largato', 'Spork')
 computador = randint(0, 4)
-print ('O computador escolheu {}'.format(itens[computador]))
+print('''Suas opções:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA
+[ 3 ] LARGATO
+[ 4 ] SPORK''')
+jogador = int(input('Qual é a sua jogada?'))
+print('-=' * 10)
+print ('Computador jogou {}'.format(itens[computador]))
+print ('Jogadou jogou {}'.format(itens[jogador]))
+print('-=' * 10)
