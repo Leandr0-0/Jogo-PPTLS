31c6e1cbcdd08036007fc59e74da5900b084985f
Leandro Silva <sr.syllw@gmail.com>
2023-03-16 08:42:26 -0300

    Regras do Jogo

--- ASSET/PY/app.py
+++ ASSET/PY/app.py
@@ -1,3 +1,17 @@
+# REGRAS DO JOGO:
+# Pedra esmaga tesoura e largato;
+# Papel cobre pedra e refuta Spock;
+# Tesoura corta papel e decapita largato;
+# Largato come papel e envenena Spock;
+# Spock esmaga tesoua e vaporiza papel.
+
+# RESUMO:
+# Pedra > Tesoura e Largato;
+# Papel > Pedra e Spock;
+# Tesoura > Papel e Largato;
+# Largato > Papel e Spock;
+# Spock > Pedra e Tesoura.
+
 from random import randint 
 itens = ('Pedra', 'Papel', 'Tesoura', 'Largato', 'Spork')
 computador = randint(0, 4)
@@ -6,7 +20,7 @@ print('''Suas opções:
 [ 1 ] PAPEL
 [ 2 ] TESOURA
 [ 3 ] LARGATO
-[ 4 ] SPORK''')
+[ 4 ] SPOCK''')
 jogador = int(input('Qual é a sua jogada?'))
 print('-=' * 10)
 print ('Computador jogou {}'.format(itens[computador]))
